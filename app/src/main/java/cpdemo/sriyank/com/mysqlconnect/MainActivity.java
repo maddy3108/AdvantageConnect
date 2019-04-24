8d5313c79af92671d0e31ef0143acd629f8e9175
DELL <suraj.dreamiit@gmail.com>
2019-04-24 19:05:16 +0530

    Added the update and delete module which are working fine/..

--- app/src/main/java/cpdemo/sriyank/com/mysqlconnect/MainActivity.java
+++ app/src/main/java/cpdemo/sriyank/com/mysqlconnect/MainActivity.java
@@ -14,10 +14,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.squareup.picasso.Picasso;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -30,16 +29,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<ClassListItems> itemArrayList;  //List items Array
+    private ArrayList <ClassListItems> itemArrayList;  //List items Array
     private MyAppAdapter myAppAdapter; //Array Adapter
     private ListView listView; // ListView
     private boolean success = false; // boolean
-
-    //private static final String DB_URL = "jdbc:mysql://DATABASE_IP/DATABASE_NAME";
-    private static final String DB_URL = "jdbc:mysql://192.168.43.159:3306/my_library"; //"jdbc:mysql://DATABASE_IP/DATABASE_NAME";
-    private static final String USER = "root";
-    private static final String PASS = "password";
-
+    public static Connection conn;
     @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.listView); //ListView Declaration
-        itemArrayList = new ArrayList<ClassListItems>(); // Arraylist Initialization
+        itemArrayList = new ArrayList <ClassListItems>(); // Arraylist Initialization
 
         // Calling Async Task
         SyncData orderData = new SyncData();
@@ -56,134 +50,150 @@ public class MainActivity extends AppCompatActivity {
 
     // Async Task has three overrided methods,
     @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
-    private class SyncData extends AsyncTask<String, String, String> {
+    private class SyncData extends AsyncTask <String, String, String> {
+           ProgressDialog progressDialog;
         String msg = "Internet/DB_Credentials/Windows_FireWall_TurnOn Error, See Android Monitor in the bottom For details!";
 
         @Override
         protected void onPreExecute() //Starts the progress dailog
         {
+            progressDialog=new ProgressDialog(MainActivity.this);
+            progressDialog.show();
+            progressDialog.setMessage("Synchronizing");
+
         }
 
         @Override
         protected String doInBackground(String... strings)  // Connect to the database, write query and add items to array list
         {
-            Connection conn=null;
+
+             conn = null;
             try {
-                Class.forName("com.extendedsystems.jdbc.advantage.ADSDriver");
-                try {
-                    conn = DriverManager.getConnection("jdbc:extendedsystems:advantage://192.168.43.159:6262;catalog = C:\\AdsBOOK\\DemoDictionary.add;Showdeleted=false;Tabletype=adt;user=adssys;password=password");                }
-                catch(Exception ex)
-                {
-                    Log.i("MyError",ex.getLocalizedMessage());
-                }
-                Log.i("Connected","Connected");
-                if (conn == null) {
-                    success = false;
-                } else {
-                    // Change below query according to your own database.
-                    String query = "SELECT [First name],[First name] FROM customer";
-                    Statement stmt = conn.createStatement();
-                    ResultSet rs = stmt.executeQuery(query);
-                    if (rs != null) // if resultset not null, I add items to itemArraylist using class created
-                    {
-                        while (rs.next()) {
-                            try {
-                                itemArrayList.add(new ClassListItems(rs.getString("First name"), rs.getString("First name")));
-                            } catch (Exception ex) {
-                                ex.printStackTrace();
-                            }
-                        }
-                        msg = "Found";
-                        success = true;
-                    } else {
-                        msg = "No Data found!";
-                        success = false;
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                Writer writer = new StringWriter();
-                Log.i("Mymessage",e.getLocalizedMessage());
 
-                Log.i("Notconnected","Notconnected");
-                e.printStackTrace(new PrintWriter(writer));
-                msg = writer.toString();
+
+                conn = Connect.coonectdb();
+                Log.i("Value is", String.valueOf(conn));
+            Log.i("Connected", "Connected");
+            if (conn == null) {
                 success = false;
-            }
-            return msg;
-        }
-
-        @Override
-        protected void onPostExecute(String msg) // disimissing progress dialoge, showing error and setting up my ListView
-        {
-            Toast.makeText(MainActivity.this, msg + "", Toast.LENGTH_LONG).show();
-            if (success == false) {
             } else {
-                try {
-                    myAppAdapter = new MyAppAdapter(itemArrayList, MainActivity.this);
-                    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                    listView.setAdapter(myAppAdapter);
-                } catch (Exception ex) {
+                // Change below query according to your own database.
+                String query = "SELECT [Product name],[Product name] FROM products";
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
 
+                if (rs != null) // if resultset not null, I add items to itemArraylist using class created
+                {
+                    while (rs.next()) {
+                        try {
+                            itemArrayList.add(new ClassListItems(rs.getString("Product name"), rs.getString("Product name")));
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+
+                    }
+                    msg = "Found";
+                    success = true;
+                } else {
+                    msg = "No Data found!";
+                    success = false;
                 }
-
             }
-        }
-    }
+        } catch(Exception e)
 
-    public class MyAppAdapter extends BaseAdapter         //has a class viewholder which holds
-    {
-        public class ViewHolder {
-            TextView textName;
-            ImageView imageView;
-        }
-
-        public List<ClassListItems> parkingList;
-
-        public Context context;
-        ArrayList<ClassListItems> arraylist;
-
-        private MyAppAdapter(List<ClassListItems> apps, Context context) {
-            this.parkingList = apps;
-            this.context = context;
-            arraylist = new ArrayList<ClassListItems>();
-            arraylist.addAll(parkingList);
-        }
-
-        @Override
-        public int getCount() {
-            return parkingList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(final int position, View convertView, ViewGroup parent) // inflating the layout and initializing widgets
         {
+            e.printStackTrace();
+            Writer writer = new StringWriter();
+            Log.i("Mymessage", e.getLocalizedMessage());
 
-            View rowView = convertView;
-            ViewHolder viewHolder = null;
-            if (rowView == null) {
-                LayoutInflater inflater = getLayoutInflater();
-                rowView = inflater.inflate(R.layout.list_content, parent, false);
-                viewHolder = new ViewHolder();
-                viewHolder.textName = (TextView) rowView.findViewById(R.id.textName);
-                rowView.setTag(viewHolder);
-            } else {
-                viewHolder = (ViewHolder) convertView.getTag();
+            Log.i("Notconnected", "Notconnected");
+            e.printStackTrace(new PrintWriter(writer));
+            msg = writer.toString();
+            success = false;
+        }
+            return msg;
+    }
+
+    @Override
+    protected void onPostExecute(String msg) // disimissing progress dialoge, showing error and setting up my ListView
+    {
+        Toast.makeText(MainActivity.this, msg + "", Toast.LENGTH_LONG).show();
+        progressDialog.dismiss();
+        if (success == false) {
+        } else {
+            try {
+                myAppAdapter = new MyAppAdapter(itemArrayList, MainActivity.this);
+                listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                listView.setAdapter(myAppAdapter);
+            } catch (Exception ex) {
+
             }
-            // here setting up names and images
-            viewHolder.textName.setText(parkingList.get(position).getName() + "");
 
-            return rowView;
         }
     }
+}
+
+public class MyAppAdapter extends BaseAdapter         //has a class viewholder which holds
+{
+    public class ViewHolder {
+        TextView textName;
+        ImageView imageView;
+    }
+
+    public List <ClassListItems> parkingList;
+
+    public Context context;
+    ArrayList <ClassListItems> arraylist;
+
+    private MyAppAdapter(List <ClassListItems> apps, Context context) {
+        this.parkingList = apps;
+        this.context = context;
+        arraylist = new ArrayList <ClassListItems>();
+        arraylist.addAll(parkingList);
+    }
+
+    @Override
+    public int getCount() {
+        return parkingList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) // inflating the layout and initializing widgets
+    {
+
+        View rowView = convertView;
+        ViewHolder viewHolder = null;
+        if (rowView == null) {
+            LayoutInflater inflater = getLayoutInflater();
+            rowView = inflater.inflate(R.layout.list_content, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.textName = (TextView) rowView.findViewById(R.id.textName);
+            rowView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        // here setting up names and images
+        viewHolder.textName.setText(parkingList.get(position).getName() + "");
+        viewHolder.textName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),parkingList.get(position).getName(),Toast.LENGTH_LONG).show();
+                Updata updata=new Updata();
+                updata.show(getSupportFragmentManager(),"Update data");
+
+            }
+        });
+        return rowView;
+    }
+}
 }
